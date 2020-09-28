b35cbeda7965090b4b14de47fb6003bf86ca76d3
ggafiled <gafewkik234@gmail.com>
2020-09-28 12:39:53 +0700

    using app script env.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { doPost } from './server/webapp';
-import './sheets.config';
+
+// eslint-disable-next-line no-global-assign
+Logger = BetterLog.useSpreadsheet(
+  PropertiesService.getScriptProperties().getProperty('GOOGLE_SHEET_ID').toString()
+);
+Tamotsu.initialize();
 
 global.doPost = doPost;
--- src/sheets.config.js
+++
@@ -1,4 +0,0 @@
-const sheetId = '1Bmlho36qViadovtJjN8URM71OInpOW7vdv1X3dWp4Vg';
-// eslint-disable-next-line no-global-assign
-Logger = BetterLog.useSpreadsheet(sheetId);
-Tamotsu.initialize();
